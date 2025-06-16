7a78ee71d1c1426a54321fa158888758a526ad26
Emmanuil Galihanov <emikgal2507@gmail.com>
2025-06-16 18:45:31 +0500

    Написаны модели

---
+++ main/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Предмет')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Предмет'
+        verbose_name_plural = 'Предметы'
+
+class Material(models.Model):
+    class MaterialType(models.TextChoices):
+        LAB = 'laba', 'Лабораторная работа'
+        INDEP = 'independent ', 'Самостоятельная работа'
+        LECTURE = 'lecture', 'Лекция'
+
+    name = models.CharField(max_length=100, verbose_name='Материал')
+    slug = models.SlugField(max_length=100)
+    subject = models.ForeignKey('main.Subject', on_delete=models.CASCADE)
+    type = models.CharField(max_length=15, choices=MaterialType, verbose_name='Тип материала')
+    content = models.JSONField(verbose_name='Условие')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Материал'
+        verbose_name_plural = 'Материалы'
+
+class Solution(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Решение')
+    slug = models.SlugField(max_length=100)
+    material = models.ForeignKey('main.Subject', on_delete=models.CASCADE)
+    variant = models.PositiveSmallIntegerField(verbose_name='Вариант')
+    content = models.JSONField(verbose_name='Решение')
+
+    def __str__(self):
+        if self.variant != 0:
+            return f'Решение {self.material.name} вариант {self.variant}'
+        else:
+            return f'Решение {self.material.name}'
+    
+    class Meta:
+        verbose_name = 'Решение'
+        verbose_name_plural = 'Решения'
